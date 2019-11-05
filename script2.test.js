b9af0983a0ffcc03f898f5283d3e4f16784ed461
Ridwan Balogun <balogunridwan@gmail.com>
2019-11-05 14:44:37 +0100

    Asynchronous testing

--- script2.test.js
+++ script2.test.js
@@ -5,7 +5,7 @@ const swapi = require('./script2');
 it('calls swapi to get people', () => {
     expect.assertions(1)
     return swapi.getPeopleAsync(fetch).then(data => {
-        
+
         expect(data.count).toEqual(87);
 
     })

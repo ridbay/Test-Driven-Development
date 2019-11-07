7cce8c6cb45b44534f212a2a325208cdf3af1e62
Ridwan Balogun <balogunridwan@gmail.com>
2019-11-07 18:28:42 +0100

    Mock and spies

--- script2.test.js
+++ script2.test.js
@@ -24,9 +24,11 @@ it('getPeople returns count and results', ()=>{
             results: [0,1,2,3,4,5]
         })
     }))
-    expect.assertions(2)
+    expect.assertions(4)
     return swapi.getPeoplePromise(mockFetch).then(data=> {
         expect(mockFetch.mock.calls.length).toBe(1);
         expect(mockFetch.tobeCalledWith('https://swapi.co/api/people'));
+        expect(data.count).toEqual(87)
+        expect(data.results.length).toBeGreaterThan(5);
     })
 })

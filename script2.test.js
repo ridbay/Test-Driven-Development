9afcb1853613967047b8a905bbffdf8fb41e0370
Ridwan Balogun <balogunridwan@gmail.com>
2019-11-07 18:29:21 +0100

    Mock and spies

--- script2.test.js
+++ script2.test.js
@@ -17,15 +17,15 @@ it('calls swapi to get people', () => {
     })
 })
 
-it('getPeople returns count and results', ()=>{
+it('getPeople returns count and results', () => {
     const mockFetch = jest.fn().mockReturnValue(Promise.resolve({
         json: () => Promise.resolve({
             count: 87,
-            results: [0,1,2,3,4,5]
+            results: [0, 1, 2, 3, 4, 5]
         })
     }))
     expect.assertions(4)
-    return swapi.getPeoplePromise(mockFetch).then(data=> {
+    return swapi.getPeoplePromise(mockFetch).then(data => {
         expect(mockFetch.mock.calls.length).toBe(1);
         expect(mockFetch.tobeCalledWith('https://swapi.co/api/people'));
         expect(data.count).toEqual(87)
